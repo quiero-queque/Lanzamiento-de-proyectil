350cf8a5b494be9e69dd75510b1bc126ad3d643c
samuelct-sys <samuel.cament@liceoindustriala20.cl>
2026-04-20 20:30:03 -0400

    subida 2

--- main.c
+++ main.c
@@ -1,10 +1,31 @@
 #include <stdio.h>
 #include <math.h>
-#define PI 3,1416
+#define PI 3.1416
+//probar con la funcion  M_PI
 int main(){
-    printf("Bienvenido a lanzamiento de proyectil con gravedad variable\n");
-    printf("Seleccione una opcion\n");
-    printf("1-jugar\n");
-    printf("2-Salir del juego\n");
+    int eleccion;
+    do{
+        printf("Bienvenido a lanzamiento de proyectil con gravedad variable\n");
+        printf("Seleccione una opcion\n");
+        printf("1-Jugar\n");
+        printf("2- Intrucciones\n");
+        printf("3-Salir del juego\n");
+        scanf("%d", &eleccion);
+        switch(eleccion){
+            case 1:
+            printf("Iniciando juego...\n");
+            break;
+            case 2:
+            printf("Intrucciones:\n");
+            printf("Hay que ajustar la gravedad, velocidad y angulo de lanzamiento\n");
+            printf("El numero PI esta aproximado a 3.1416\n");
+            break;
+            case 3:
+            printf("Saliendo del juego...");
+            break;
+            default:
+            printf("Opcion invalida\n");
+            }
+    }while(eleccion!=3);
     return 0;
 }
