7db7ff7681af5b39048f2dd38e920eecf2549a7a
samuelct <samuel.cament@liceoindustriala20.cl>
2026-04-24 23:19:41 -0400

    Change eleccion type and update input handling

--- src/main.c
+++ src/main.c
@@ -1,30 +1,30 @@
 #include "funciones.h"
 //probar con la funcion  M_PI(los include son locales de cada archivo)
 int main(){
-    int eleccion;
+    char eleccion;
     do{
         printf("Bienvenido a lanzamiento de proyectil con gravedad variable\n");
         printf("Seleccione una opcion\n");
         printf("1-Jugar\n");
         printf("2- Intrucciones\n");
         printf("3-Salir del juego\n");
-        scanf("%d", &eleccion);
+        scanf("%s", &eleccion);
         switch(eleccion){
-            case 1:
+            case '1':
             printf("Iniciando juego...\n");
-            void iniciar_juego();
+            iniciar_juego();
             break;
-            case 2:
+            case '2':
             printf("Intrucciones:\n");
             printf("Hay que ajustar la gravedad, velocidad y angulo de lanzamiento\n");
             printf("El numero PI esta aproximado a 3.1416\n");
             break;
-            case 3:
+            case '3':
             printf("Saliendo del juego...");
             break;
             default:
             printf("Opcion invalida\n");
             }
-    }while(eleccion!=3);
+    }while(eleccion!='3');
     return 0;
 }
