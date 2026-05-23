b630185c0dcd933e6fdffab2ffbb76f284779d47
Francisco Condori <103710712+PanchoPe@users.noreply.github.com>
2026-05-23 19:59:54 -0400

    Limpieza de pantalla - Instrucciones
    Agregar las líneas "clear", para limpiar pantalla cada vez que se ejecute una opción, y las instrucciones fueron agregadas como una función.

--- src/main.c
+++ src/main.c
@@ -1,29 +1,35 @@
 #include "funciones.h"
+#include <stdlib.h>
 int main(){
     char eleccion;
     do{
+        system("clear");
         printf("Bienvenido a lanzamiento de proyectil con gravedad variable\n");
         printf("Seleccione una opcion\n");
         printf("1-Jugar\n");
-        printf("2- Intrucciones\n");
+        printf("2-Intrucciones\n");
         printf("3-Salir del juego\n");
-        scanf("%s", &eleccion);
+        scanf("%c", &eleccion);
         switch(eleccion){
-            case '1':
-            printf("Iniciando juego...\n");
-            iniciar_juego();
-            break;
-            case '2':
-            printf("Intrucciones:\n");
-            printf("Hay que ajustar la gravedad, velocidad y angulo de lanzamiento\n");
-            printf("El numero PI esta aproximado a 3.1416\n");
-            break;
-            case '3':
-            printf("Saliendo del juego...");
-            break;
-            default:
-            printf("Opcion invalida\n");
+                case '1':
+                system("clear");
+                printf("Iniciando juego...\n");
+                iniciar_juego();
+                break;
+                case '2':
+                system("clear");
+                instrucciones(eleccion);
+                break;
+                case '3':
+                system("clear");
+                printf("Saliendo del juego...\n");
+                break;
+                default:
+                system("clear");
+                printf("Opcion invalida\n");
+                break;
             }
-    }while(eleccion!='3');
+    }while(eleccion != '3');
     return 0;
 }
+
