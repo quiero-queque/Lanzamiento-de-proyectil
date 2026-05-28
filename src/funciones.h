e3f96a6a47e9e44ecbc06e6dd5f91ac606fd0d92
Benyamin <benjamin.vargasaz11@gmail.com>
2026-05-28 12:17:29 -0400

    Update funciones.h

--- src/funciones.h
+++ src/funciones.h
@@ -1,21 +1,16 @@
 #include <stdio.h>
 #include <math.h>
-#define PI 3.1416
-#define TIERRA 9.81
-#define VENUS 8.87
-#define MERCURIO 3.70
-#define MARTE 3.71
-#define JUPITER 24.79
-#define SATURNO 10.44
-#define URANO 8.69
-#define NEPTUNO 11.15
-#define LUNA 1.62
 //los angulos se reciben en grados y se cambian a radianes
 float calcular_alcance(float velocidad, float angulo, float t);
+//funciones fisicas
 float calcular_altura(float velocidad, float angulo, float t, float g);
 float calcular_tiempo(float velocidad, float angulo, float g);
 float calcular_radianes(float Angulo);
 float calcular_alturamax(float velocidad, float angulo, float g);
-void instrucciones(char eleccion);
+float calcular_alcancemax(float velocidad, float angulo, float g);
+//juego con preguntas
 void iniciar_juego();
-
+// pruebas donde se seleccionan los planetas
+void modolibre();
+//solucion bug
+float limpiarbuffer(const char *instruccion);
