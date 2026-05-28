429f938e6181c9a6286ab9fab3e94a7f510bafd2
Benyamin <benjamin.vargasaz11@gmail.com>
2026-05-28 12:22:18 -0400

    Update main.c
    Se cambio el orden y se agrego el modo libre

--- src/main.c
+++ src/main.c
@@ -1,37 +1,35 @@
 #include "funciones.h"
-#include <stdlib.h>
+//probar con la funcion  M_PI(los include son locales de cada archivo)
 int main(){
     char eleccion;
     do{
-        system("clear");
         printf("Bienvenido a lanzamiento de proyectil con gravedad variable\n");
         printf("Seleccione una opcion\n");
-        printf("1-Jugar\n");
-        printf("2-Intrucciones\n");
-        printf("3-Salir del juego\n");
-        scanf("%c", &eleccion);
-        //solucion bug
-        while(getchar() != '\n');
+        printf("1-Intrucciones\n");
+        printf("2-Jugar\n");
+        printf("3-Modo libre\n");
+        printf("4-Salir del juego\n");
+        scanf(" %c", &eleccion);
         switch(eleccion){
-                case '1':
-                system("clear");
-                printf("Iniciando juego...\n");
-                iniciar_juego();
-                break;
-                case '2':
-                system("clear");
-                instrucciones(eleccion);
-                break;
-                case '3':
-                system("clear");
-                printf("Saliendo del juego...\n");
-                break;
-                default:
-                system("clear");
-                printf("Opcion invalida\n");
-                break;
+            case '1':
+            printf("Intrucciones:\n");
+            printf("Hay que ajustar la gravedad, velocidad y angulo de lanzamiento\n");
+            printf("El numero PI esta aproximado a 3.1416\n");
+            break;
+            case '2':
+            printf("Iniciando juego...\n");
+            iniciar_juego();
+            break;
+            case '3':
+            printf("Entrando a modo libre");
+            modolibre();
+            break;
+            case '4':
+            printf("Saliendo del juego...");
+            break;
+            default:
+            printf("Opcion invalida\n");
             }
-    }while(eleccion != '3');
+    }while(eleccion!='4');
     return 0;
 }
-
